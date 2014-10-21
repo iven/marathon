977cc70986d00402b212d51040ab838ab8cc3e08
Xu Lijian <xulijian@qiyi.com>
2014-10-21 11:16:46 +0800

    Add `hostname` support for `GROUP_BY` constraint
    Change-Id: I0db2a35867763e20008686d2a6030cdd03154ace

--- src/main/scala/mesosphere/mesos/Constraints.scala
+++ src/main/scala/mesosphere/mesos/Constraints.scala
@@ -38,33 +38,22 @@ object Constraints {
         false
       }
 
-    private def checkGroupBy = {
+    private def checkGroupBy(constraintValue: String, groupFunc: (MarathonTask) => Option[String]) = {
+      // Minimum group count
       val minimum = List(GroupByDefault, getIntValue(value, GroupByDefault)).max
-      // Group tasks by the constraint value
-      val groupedTasks = tasks.groupBy(
-        _.getAttributesList.asScala
-          .find(_.getName == field)
-          .map(_.getText.getValue)
-      )
-
-      // Order groupings by smallest first
-      val orderedByCount = groupedTasks.toSeq.sortBy(_._2.size)
-      def minValue = orderedByCount.headOption.map(_._1).getOrElse("")
+      // Group tasks by the constraint value, and calculate the task count of each group
+      val groupedTasks = tasks.groupBy(groupFunc).mapValues(_.size)
+      // Task count of the smallest group
+      val minCount = groupedTasks.values.reduceOption(_ min _).getOrElse(0)
 
       // Return true if any of these are also true:
       // a) this offer matches the smallest grouping when there
       // are >= minimum groupings
       // b) the constraint value from the offer is not yet in the grouping
-      val condA =
-        orderedByCount.size >= minimum &&
-          // true if the smallest group has this attribute value
-          (minValue == attr.get.getText.getValue ||
-            // or all groups are the same size
-            orderedByCount.headOption.map(_._2.size) == orderedByCount.lastOption.map(_._2.size))
-
-      def condB = !orderedByCount.exists(_._1.exists(_ == attr.get.getText.getValue))
-
-      condA || condB
+      groupedTasks.find(_._1.exists(_ == constraintValue)) match {
+        case Some(pair) => (groupedTasks.size >= minimum) && (pair._2 == minCount)
+        case None       => true
+      }
     }
 
     private def checkHostName =
@@ -73,6 +62,7 @@ object Constraints {
         case Operator.UNLIKE => !offer.getHostname.matches(value)
         // All running tasks must have a hostname that is different from the one in the offer
         case Operator.UNIQUE => tasks.forall(_.getHost != offer.getHostname)
+        case Operator.GROUP_BY => checkGroupBy(offer.getHostname, (task: MarathonTask) => Option(task.getHost))
         case Operator.CLUSTER =>
           // Hostname must match or be empty
           (value.isEmpty || value == offer.getHostname) &&
@@ -89,7 +79,13 @@ object Constraints {
           (value.isEmpty || attr.get.getText.getValue == value) &&
             // All running tasks should have the matching attribute
             matches.size == tasks.size
-        case Operator.GROUP_BY => checkGroupBy
+        case Operator.GROUP_BY => {
+          val groupFunc = (task: MarathonTask) =>
+            task.getAttributesList.asScala
+              .find(_.getName == field)
+              .map(_.getText.getValue)
+          checkGroupBy(attr.get.getText.getValue, groupFunc)
+        }
         case Operator.LIKE =>
           if (value.nonEmpty) {
             attr.get.getText.getValue.matches(value)
